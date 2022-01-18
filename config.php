01d85514bf13f5df366b443907e36691b8938792
alvinvoonyn <alvinvoonyn@yahoo.com>
2022-01-18 12:29:10 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', getenv('DB_SERVER'));
 define('DB_USERNAME', getenv('MYSQL_USER'));
 define('DB_PASSWORD', getenv('MYSQL_PASSWORD'));
-define('DB_NAME', getenv('DB_NAME'));
+define('DB_NAME', getenv('MYSQL_DATABASE'));
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
